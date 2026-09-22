7d83e306185ee6bf5d182441ab8d8b437a455590
Fernando Moralles <fhmoralles@gmail.com>
2026-09-21 21:38:12 -0300

    main: frontend update.

--- frontend/dist/frontend/server/angular-app-manifest.mjs
+++ frontend/dist/frontend/server/angular-app-manifest.mjs
@@ -25,9 +25,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 11928, hash: 'd4d881524b220c7bc950487e3f8b226959165578ecf8739ff4a588397e4cb2a4', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 12114, hash: '3d45edb524d7bee37797b6616ddc7551707a832dd146c373a4de2942c49cde9f', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 28251, hash: '26e780fa7223d9fd21e97902e5b075ff2ca63932c363c3bd351ba463dbaf6a4f', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 11928, hash: 'b98a6c0789e0a483c156ac87b5731abb87ef420e249189c28d755ac28a2ca404', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 12114, hash: '66a35a1c2639eadbf10d2fd01ea49a1229f2dbbc5d2fb38d9e1d2689b4722e65', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 28447, hash: 'fffdabae53821aab4aafdb130f939501e60edda7c085ea50cf690dc3a7edce37', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-HXK7QCQ7.css': {size: 202, hash: 'dVeOGgstMB0', text: () => import('./assets-chunks/styles-HXK7QCQ7_css.mjs').then(m => m.default)}
   },
 };
