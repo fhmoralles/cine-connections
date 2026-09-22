52c42818fefac007dce61342f720eb89a496eb7b
Fernando Moralles <fhmoralles@gmail.com>
2026-09-21 21:40:41 -0300

    main: version 1.0.0.0001

--- frontend/src/environments/environment.production.ts
+++ frontend/src/environments/environment.production.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
 
-  version: '1.0.0',
+  version: '1.0.0.0001',
 
   apiUrl: '/api',
 
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
 
-  version: '1.0.0',
+  version: '1.0.0.0001',
 
   apiUrl: 'http://localhost:8080/api',
 
